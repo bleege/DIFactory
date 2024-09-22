aa82255efaf6e2b186e8646d99f8ad6c75a75a37
Brad Leege <bleege@gmail.com>
2024-09-22 13:29:49 -0500

    Initial Commit

---
+++ DIFactory/DIFactoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIFactoryApp.swift
+//  DIFactory
+//
+//  Created by Brad Leege on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct DIFactoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
